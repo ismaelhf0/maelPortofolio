893cca9f47917783e06d88989c95d54270e0d57f
ismailhf0 <ismail.hajfani@gmail.com>
2024-07-02 21:29:57 +0100

    made the a responsive navbar with the backdrop, the colors and the styles/font are not setup, i need to add mixin first

---
+++ src/components/header/navbar.jsx
@@ -0,0 +1,72 @@
+import { NavLink } from 'react-router-dom'
+import logo from './../../assets/images/logo.png'
+import './navbar.scss'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+  const [shownavbar, setshownavbar] = useState(false)
+  const handleShowNavbar = () => {
+    setshownavbar(!shownavbar)
+  }
+  const handleClickedNavLink = () => {
+    setshownavbar(!shownavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div
+        className={`blur ${shownavbar && 'active'}`}
+        onClick={handleShowNavbar}
+      ></div>
+
+      <div className="container">
+        <div className="logo">
+          <NavLink className="home" to={'/'}>
+            <img className="brand " src={logo} alt="logo" />
+          </NavLink>
+        </div>
+
+        <div
+          className={`OpenMenuIcon ${shownavbar && 'active'}`}
+          onClick={handleShowNavbar}
+        >
+          <FontAwesomeIcon className="FaMenuIcon" icon={faBars} />
+        </div>
+
+        <div
+          id="menu"
+          className={`nav-elements-container ${shownavbar && 'active'}`}
+        >
+          <div
+            className={`CloseMenuIcon ${shownavbar && 'active'}`}
+            onClick={handleShowNavbar}
+          >
+            <FontAwesomeIcon className="FaCloseIcon" icon={faXmark} />
+          </div>
+
+          <ul className="navUl">
+            <li>
+              <NavLink onClick={handleClickedNavLink} to={'/project'}>
+                project
+              </NavLink>
+            </li>
+            <li>
+              <NavLink onClick={handleClickedNavLink} to={'/about'}>
+                about
+              </NavLink>
+            </li>
+            <li>
+              <NavLink onClick={handleClickedNavLink} to={'/contact'}>
+                contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
